f1a785a682d6ada9b622aa7fd628fa5bb7ec0e66
LuisHenrii <luishenriquefonsecaphp@gmail.com>
2023-12-05 01:58:01 -0300

    git das 2h da manha

--- minhas_informacoes.php
+++ minhas_informacoes.php
@@ -69,27 +69,29 @@ $usuario = $consulta->fetch();
 
     </main>
 
-    <!-- RODAPE -->
-    <footer class="py-5" id="footer">
-        <div class="row justify-content-center">
-            <div class="col-6 col-md-2 mb-2">
-                <h5>Sobre Nós</h5>
-                <ul class="nav flex-column">
-                    <li class="nav-item mb-2"><a href="sobre_nos.php" class="nav-link p-0 text-body-secondary">Sobre
-                            Nós</a></li>
-                    <li class="nav-item mb-2"><a href="como_funciona.php" class="nav-link p-0 text-body-secondary">Como
-                            Funciona?</a></li>
-                    <li class="nav-item mb-2"><a href="comunidade.php"
-                            class="nav-link p-0 text-body-secondary">Comunidade</a></li>
-                </ul>
-            </div>
 
-            <div class="col-6 col-md-2 mb-2">
-                <h5>Informações</h5>
-                <ul class="nav flex-column">
-                    <li class="nav-item mb-2"><a href="contato.php" class="nav-link p-0 text-body-secondary">Contato</a>
-                    </li>
-                </ul>
+    <!-- RODAPE -->
+    <footer id="footer">
+
+        <div class="container">
+            <div class="h1" id="achados_perdidos">
+                <h1>Achados&Perdidos</h1>
+
+            </div>
+            <div style="display: flex; align-items: center;">
+                <img class="icon" src="img/icons8-phone-48.png"
+                    style="width: 25px; height: 25px;  margin-bottom:17px; padding-right: 5px;" alt="icon">
+                <p>+55 5398405-5364</p>
+            </div>
+            <div style="display: flex; align-items: center;">
+                <img class="icon" src="img/icons8-gmail-50.png"
+                    style="width: 25px; height: 25px; margin-bottom:19px; padding-right: 5px;" alt="icon">
+                <p>luishenriquefonsecaphp@gmail.com</p>
+            </div>
+            <div style="display: flex; align-items: center;">
+                <img class="icon" src="img/icons8-location-60.png"
+                    style="width: 25px; height: 25px; margin-bottom:19px; padding-right: 5px;" alt="icon">
+                <p>Pelotas, Rio Grande Do Sul</p>
             </div>
         </div>
     </footer>
