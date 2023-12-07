892a74f67e18f4fe1b598983a4651b5e07854c20
LuisHenrii <luishenriquefonsecaphp@gmail.com>
2023-12-07 18:44:19 -0300

    alteracao no registro

--- registro.php
+++ registro.php
@@ -9,6 +9,7 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
 $msg = "";
+$aviso ="";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nome = $_POST["nome"];
@@ -16,12 +17,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $senha = $_POST["senha"];
 
     if ($email === false) {
-        echo "E-mail inválido. Por favor, insira um e-mail válido. <hr>";
+        $aviso = "E-mail inválido. Por favor, insira um e-mail válido. <hr>";
 
     }
 
     if (strlen($senha) < 8) {
-        echo "A senha deve ter pelo menos 8 caracteres.";
+        $aviso = "A senha deve ter pelo menos 8 caracteres.";
     } else {
         try {
             $verificaEmail = $pdo->prepare("SELECT COUNT(*) FROM pessoa WHERE email = ?");
@@ -29,7 +30,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $emailExistente = (int) $verificaEmail->fetchColumn();
 
             if ($emailExistente > 0) {
-                echo "O e-mail já está registrado. Por favor, use outro e-mail.";
+                $aviso = "O e-mail já está registrado. Por favor, use outro e-mail.";
             } else {
                 $codigoVerificacao = bin2hex(random_bytes(16));
                 $senhaHash = password_hash($senha, PASSWORD_DEFAULT);
@@ -61,11 +62,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                     $msg = "Cadastrado! Verifique seu e-mail para continuar.";
 
                 } catch (Exception $e) {
-                    echo "Erro ao enviar o e-mail";
+                    $aviso = "Erro ao enviar o e-mail";
                 }
             }
         } catch (PDOException $e) {
-            echo "Erro no registro: " . $e->getMessage();
+            $aviso = "Erro no registro: " . $e->getMessage();
         }
     }
 
@@ -78,11 +79,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <div id="conteudoRegistro" class="container justfy-content-center">
             <h2>Registro</h2>
             <form action="registro.php" method="POST">
+
                 <?php
                 if (!empty($msg)) {
                     echo '<div class="alert alert-success">' . $msg . '</div>';
                 }
+                if (!empty($aviso)) {
+                    echo '<div class="alert alert-warning">' . $aviso . '</div>';
+                }
+
                 ?>
+
                 <div class="mb-3">
                     <label for="nome" class="form-label">Nome de Usuário:</label>
                     <input type="text" class="form-control" id="nome" name="nome" required>
@@ -90,16 +97,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 </div>
 
                 <div class="mb-3">
-                    <label for="email" class="form-label">E-mail (somente @gmail.com):</label>
-                    <input type="email" class="form-control" id="email" name="email"
+                    <label for="email" class="form-label">E-mail:</label>
+                    <input type="email" class="form-control" id="email" name="email"placeholder="exemplo@gmail.com"
                         pattern="[a-zA-Z0-9._%+-]+@gmail\.com$" required>
-                    <small class="form-text text-muted">Digite um e-mail válido que termine com @gmail.com.</small>
                     <hr>
                 </div>
 
                 <div class="mb-3">
-                    <label for="senha" class="form-label">Senha (8 caracteres ou mais):</label>
-                    <input type="password" class="form-control" id="senha" name="senha" required minlength="8">
+                    <label for="senha" class="form-label">Senha:</label>
+                    <input type="password" class="form-control" id="senha" name="senha" placeholder="Senha de 8 caracteres ou mais" required minlength="8">
                     <hr>
                 </div>
 
