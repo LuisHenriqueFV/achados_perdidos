7f0261f8648cf918ba90dc8029c8354f8ea67043
LuisHenrii <luishenriquefonsecaphp@gmail.com>
2023-12-06 21:52:11 -0300

    ok

--- login.php
+++ login.php
@@ -5,11 +5,11 @@ $_SESSION["msg"] = "";
 
 require("./includes/components/funcao.php");
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+if($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST["email"];
     $senha = $_POST["password"];
 
-    if (!(empty($email) || empty($senha))) {
+    if(!(empty($email) || empty($senha))) {
 
         require('./includes/components/conecta.php');
 
@@ -19,14 +19,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         $usuario = $consulta->fetch(PDO::FETCH_ASSOC);
 
-        
 
-        if ($usuario) {
+
+        if($usuario) {
             $verificado = $usuario['verificado'];
 
-            if ($verificado) {
+            if($verificado) {
                 $senhaBanco = $usuario['senha'];
-                if (password_verify($senha, $senhaBanco)) {
+                if(password_verify($senha, $senhaBanco)) {
                     $_SESSION["logged"] = true;
 
                     $_SESSION["codpessoa"] = $usuario['codpessoa'];
@@ -56,15 +56,18 @@ require("./includes/components/cabecalho.php");
 
 <body>
 
+   
     <main>
-        
+
+
+
         <div id="loginContainer" class="container">
 
             <div class="form-container rounded-4">
                 <form action="login.php" method="POST" class="needs-validation">
-
+                 
                     <div class="text-center mb-4">
-                        <h1 class="mb-2 fw-regular">Login</h1>
+                        <h2 class="mb-2 fw-regular">Login</h2>
                         <hr class="w-100 mx-auto my-2">
                     </div>
 
@@ -86,7 +89,7 @@ require("./includes/components/cabecalho.php");
                             <p><a href="registro.php"
                                     class="link-secondary link-offset-2 link-opacity-25 link-opacity-100-hover">Registre-se</a>
                             </p>
-                        
+
                         </div>
                         <div class="col-md-6 text-center">
                             <p><a href="recuperar_senha.php"
@@ -94,13 +97,12 @@ require("./includes/components/cabecalho.php");
                                     a Senha</a></p>
                         </div>
                     </div>
-                    <hr>
 
                     <?php echo $_SESSION["msg"]; ?>
                 </form>
 
                 <?php
-                if (isset($_SESSION['senha_alterada'])) {
+                if(isset($_SESSION['senha_alterada'])) {
                     echo '<p style="color: green;">Senha alterada com sucesso!</p>';
                     unset($_SESSION['senha_alterada']);
                 }
@@ -110,36 +112,36 @@ require("./includes/components/cabecalho.php");
         </div>
         <!-- FIM DO CONTEUDO -->
     </main>
-  
+
 
 </body>
 
-    <!-- RODAPE -->
-    <footer id="footer">
+<!-- RODAPE -->
+<footer id="footer">
 
-        <div class="container">
-            <div class="h1" id="achados_perdidos">
+    <div class="container">
+        <div class="h1" id="achados_perdidos">
             <h1>Achei!</h1>
 
-            </div>
-            <div style="display: flex; align-items: center;">
-                <img class="icon" src="img/icons8-phone-48.png"
-                    style="width: 25px; height: 25px;  margin-bottom:17px; padding-right: 5px;" alt="icon">
-                <p>+55 5398405-5364</p>
-            </div>
-            <div style="display: flex; align-items: center;">
-                <img class="icon" src="img/icons8-gmail-50.png"
-                    style="width: 25px; height: 25px; margin-bottom:19px; padding-right: 5px;" alt="icon">
-                <p>luishenriquefonsecaphp@gmail.com</p>
-            </div>
-            <div style="display: flex; align-items: center;">
-                <img class="icon" src="img/icons8-location-60.png"
-                    style="width: 25px; height: 25px; margin-bottom:19px; padding-right: 5px;" alt="icon">
-                <p>Pelotas, Rio Grande Do Sul</p>
-            </div>
         </div>
-    </footer>
+        <div style="display: flex; align-items: center;">
+            <img class="icon" src="img/icons8-phone-48.png"
+                style="width: 25px; height: 25px;  margin-bottom:17px; padding-right: 5px;" alt="icon">
+            <p>+55 5398405-5364</p>
+        </div>
+        <div style="display: flex; align-items: center;">
+            <img class="icon" src="img/icons8-gmail-50.png"
+                style="width: 25px; height: 25px; margin-bottom:19px; padding-right: 5px;" alt="icon">
+            <p>luishenriquefonsecaphp@gmail.com</p>
+        </div>
+        <div style="display: flex; align-items: center;">
+            <img class="icon" src="img/icons8-location-60.png"
+                style="width: 25px; height: 25px; margin-bottom:19px; padding-right: 5px;" alt="icon">
+            <p>Pelotas, Rio Grande Do Sul</p>
+        </div>
+    </div>
+</footer>
 
-    <!-- FIM DO RODAPÉ -->
+<!-- FIM DO RODAPÉ -->
 
 </html>
